36d09814678e2c3bf0ff5ec9620681cb78ab159d
Baron Chong <baronchong23@gmail.com>
2025-07-05 14:06:08 +0800

    feat: add in-memory user configuration for Spring Security

---
+++ src/main/java/com/d9/bookmanager/config/InMemoryUserConfig.java
@@ -0,0 +1,28 @@
+package com.d9.bookmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class InMemoryUserConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin123")) // 加密密码
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
